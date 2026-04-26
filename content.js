3480e62cf4414d7804ff22a9c70ef488de0f2419
Dig220 <146044119+Dig220@users.noreply.github.com>
2026-04-25 23:46:49 -0400

    Add files via upload

--- content.js
+++ content.js
@@ -1,1 +1,8 @@
-const ENCRYPTED_DATA = "NhRQBu8mWGqTfmEQiP+/3Fkgy22CjhsHjZoNy30rIPycLR3AMIwQylaFEBPDtzS+XtMw8UKWjf9Mw1gVvzjxMaypicYNjLeuwiHMe2HB+tudah/8TGPK/VI919F1DQz6FbEwVpsOAO+AuCvdPuqXwTc/jqOBVSsec3KB";
+const ENTRIES = [
+  {
+    id: "entry-01",
+    publishedAt: 1745640000000,
+    expiresAt: null,
+    data: "NhRQBu8mWGqTfmEQiP+/3Fkgy22CjhsHjZoNy30rIPycLR3AMIwQylaFEBPDtzS+XtMw8UKWjf9Mw1gVvzjxMaypicYNjLeuwiHMe2HB+tudah/8TGPK/VI919F1DQz6FbEwVpsOAO+AuCvdPuqXwTc/jqOBVSsec3KB"
+  }
+];
